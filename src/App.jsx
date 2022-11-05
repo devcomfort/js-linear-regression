55d6fbfda73b8390cef8179e32fb386aa5a9c2f8
devcomfort <kimdonghyun026@gmail.com>
2022-11-05 21:38:00 +0900

    App 컴포넌트 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,25 @@
+// @ts-check
+
+import React from "react";
 import { useState } from "react";
-import {
-	Chart as ChartJS,
-	LinearScale,
-	PointElement,
-	LineElement,
-	Tooltip,
-	Legend,
-} from "chart.js";
-import { Chart } from "react-chartjs-2";
 
 import "./App.css";
 import {
 	SimpleLinearRegression,
-	sample_data,
 	MyMath,
 } from "./assets/simple_linear_regression.mjs";
+import SimpleChart from "./components/SimpleChart";
+import RandomChart from "./components/RandomChart";
+import Title from "./components/Title";
+import Description from "./components/Description";
 
 function App() {
-	ChartJS.register(LinearScale, PointElement, LineElement, Tooltip, Legend);
-
-	const vectors = {
-		x: new Array(100).fill(0).map(() => MyMath.randomNumber(-100, 100)),
-		y: new Array(100).fill(0).map(() => MyMath.randomNumber(-100, 100)),
-	};
-
-	console.log(vectors);
-	console.log(Math.min(...vectors.x), Math.max(...vectors.x));
-	console.log(Math.min(...vectors.y), Math.max(...vectors.y));
-
-	const LR = SimpleLinearRegression(vectors.x, vectors.y);
-
-	const data = {
-		datasets: [
-			{
-				type: "scatter",
-				label: "dataset",
-				data: new Array(LR.x().length).fill(0).map((_, i) => ({
-					x: LR.x()[i],
-					y: LR.y()[i],
-				})),
-				backgroundColor: "rgb(255, 99, 132)",
-			},
-			{
-				type: "line",
-				label: "line",
-				borderColor: "#535bf2",
-				borderWidth: 1,
-				fill: false,
-				data: LR.line_coords(-100, 100, 100),
-			},
-		],
-	};
-
 	return (
 		<div className="App">
-			<Chart type="scatter" data={data} height={480} width={720}></Chart>
+			<Title></Title>
+			{/* <SimpleChart></SimpleChart> */}
+			<RandomChart height={405} width={540}></RandomChart>
+			<Description></Description>
 		</div>
 	);
 }
